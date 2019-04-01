87aa999dbc82af68402b2c0159e48be383f6d203
DiamondsZz <951311326@qq.com>
2019-04-01 16:43:43 +0800

    轮播图、商品列表sql脚本文件

---
+++ sql/homecarousel.sql
@@ -0,0 +1,9 @@
+INSERT INTO `homecarousel` VALUES (1, 'http://127.0.0.1:3000/images/s1.png', NULL);
+INSERT INTO `homecarousel` VALUES (2, 'http://127.0.0.1:3000/images/s2.png', NULL);
+INSERT INTO `homecarousel` VALUES (3, 'http://127.0.0.1:3000/images/s3.png', NULL);
+INSERT INTO `homecarousel` VALUES (4, 'http://127.0.0.1:3000/images/s4.png', NULL);
+INSERT INTO `homecarousel` VALUES (5, 'http://127.0.0.1:3000/images/s5.png', NULL);
+INSERT INTO `homecarousel` VALUES (6, 'http://127.0.0.1:3000/images/s6.png', NULL);
+INSERT INTO `homecarousel` VALUES (7, 'http://127.0.0.1:3000/images/s7.png', NULL);
+INSERT INTO `homecarousel` VALUES (8, 'http://127.0.0.1:3000/images/s8.png', NULL);
+INSERT INTO `homecarousel` VALUES (9, 'http://127.0.0.1:3000/images/s9.png', NULL);
