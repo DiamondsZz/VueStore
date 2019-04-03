3e4de2220a0b90dd4fde09ee1b74a458a25ea5b0
DiamondsZz <951311326@qq.com>
2019-04-03 16:56:26 +0800

    修改nav

--- vue/src/pages/Home/Children/Hot/HotNav.vue
+++ vue/src/pages/Home/Children/Hot/HotNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hot-nav">
     <!--滚动区域-->
-    <div class="hot-nav-content">
+    <div class="hot-nav-content" ref="nav">
       <div class="nav-content-inner" v-if="homenav.length>0">
         <a class="inner-item" v-for="(item) in homenav">
           <img :src="item.iconurl" alt="">
@@ -27,11 +27,11 @@
         //屏幕宽度  window.innerWidth(ie8及以下不支持)
         screenWidth: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
         //滚动条总宽度
-        barWidth: 100,
+        barWidth: 0,
         //滚动条宽度
         barInnerWidth: 0,
         //滚动内容宽度
-        scrollContentWidth: 720,
+        scrollContentWidth: 0,
         //滚动条开始的距离
         start: 0,
         //滚动条结束的距离
@@ -55,37 +55,19 @@
     },
     methods: {
       getInnerBarWidth: function () {
-        this.barInnerWidth =this.barWidth * (this.screenWidth / this.scrollContentWidth);
+        this.barWidth = this.screenWidth / 3;
+        this.scrollContentWidth = this.screenWidth * 2;
+        this.barInnerWidth = this.barWidth * (this.screenWidth / this.scrollContentWidth);
         //console.log(this.barInnerWidth);
       },
       bindEvent: function () {
         //console.log(this);
-        this.$el.addEventListener('touchstart', this.handleTouchStart, false);
         this.$el.addEventListener('touchmove', this.handleTouchMove, false);
-        this.$el.addEventListener('touchend', this.handleTouchEnd, false);
-      },
-      handleTouchStart: function (ev) {
-         //console.log(ev);
-        this.start = ev.touches[0].pageX;
-        //console.log(ev.touches[0]);
-        //console.log(this.start);
       },
       handleTouchMove: function (ev) {
-        let scrollMoveWidth =ev.touches[0].pageX - this.start;
-        //console.log(scrollMoveWidth);
-        this.moveWidth = -(scrollMoveWidth * (this.barWidth / this.scrollContentWidth)-this.end);
-       //console.log(this.moveWidth);
-        //边界值检测
-        if (this.moveWidth >=this.barWidth - this.barInnerWidth) {
-          this.moveWidth = this.barWidth - this.barInnerWidth;
-        } else if (this.moveWidth < 0) {
-          this.moveWidth = 0;
-        }
+        this.moveWidth=( (this.barWidth - this.barInnerWidth)*this.$refs.nav.scrollLeft/this.screenWidth);
+        console.log(this.moveWidth);
 
-      },
-      handleTouchEnd: function () {
-        this.end = this.moveWidth;  //记录上一次移动的距离
-        //console.log('end:'+this.end);
       },
     }
   }
@@ -102,13 +84,14 @@
       width 100%
       overflow-x scroll
       .nav-content-inner
-        width 720px
+        width 200%
         height 180px
         display flex
         flex-wrap wrap
         .inner-item
-          width 90px
+          width 12%
           height 50%
+          margin-right 3px
           display flex
           flex-direction column
           justify-content center
@@ -119,9 +102,9 @@
             width 40%
             margin-bottom 5px
     .hot-nav-content::-webkit-scrollbar
-      display none
+      //display none
     .hot-nav-bottom
-      width 100px
+      width 33%
       height 2px
       background-color #ccc
       position absolute
